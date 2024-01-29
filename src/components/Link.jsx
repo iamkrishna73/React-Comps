e8d983c6a2d36baa4abdc26d92b6a0be0222e078
krishna <krishnasingj137333@gmail.com>
2024-01-30 00:13:26 +0530

    Handing control and command key

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -5,10 +5,13 @@ function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    if(event.metakey || event.ctrlkey) {
+      return;
+    }
     event.preventDefault();
     navigate(to);
   };
-  return <a onClick={handleClick}> {children}</a>;
+  return <a href={to} onClick={handleClick}> {children}</a>;
 }
 
 export default Link;
